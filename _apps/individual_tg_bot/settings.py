1d50d4b191186c5d3d24282d51adc513d7025e28
Aliaksandr <sigai.aleksandr@mail.ru>
2024-02-10 17:54:34 +0200

    fix(individual_tg_bot): fix settings

--- _apps/individual_tg_bot/settings.py
+++ _apps/individual_tg_bot/settings.py
@@ -11,7 +11,7 @@ ADMIN_ID = os.getenv("ADMIN_ID")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_URL = os.getenv("DB_URL")
 
-
-#Токен для оплаты через Stripe
-STRIPE_BOT_TOKEN = os.getenv('STRIPE_BOT_TOKEN')

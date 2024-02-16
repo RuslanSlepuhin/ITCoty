fda6054ee2a8dca5819e46c5169f41c06abfbf91
AliaksandrSihai <sigai.aleksandr@mail.ru>
2024-02-16 21:46:35 +0200

    build(flask_endpoint.py): add API endpoint
    Add flask endpoint from user's requests

---
+++ _apps/individual_tg_bot/flask_endpoint.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+from service import db
+
+app = Flask(__name__)
+
+
+@app.route("/user_requests_vacancies", methods=["GET"])
+async def endpoint():
+    selected_direction = request.args.get("selected_direction")[2:-2]
+    selected_specializations = request.args.get("selected_specializations")[2:-2]
+    selected_level = request.args.get("selected_level")[2:-2]
+    selected_location = request.args.get("selected_location")[2:-2]
+    selected_work_format = request.args.get("selected_work_format")[2:-2]
+    keyword = request.args.get("keyword")
+    vacancies = await db.receive_vacancy(
+        direction=selected_direction,
+        specialization=selected_specializations,
+        level=selected_level,
+        location=selected_location,
+        work_format=selected_work_format,
+        keyword=keyword,
+    )
+    return jsonify(vacancies)
+
+
+if __name__ == "__main__":
+    app.run()

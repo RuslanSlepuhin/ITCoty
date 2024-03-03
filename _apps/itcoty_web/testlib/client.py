4b7a89dea65b6f6d35cc109c72103af2ba313af8
Artsiom_PH <workartpharm@gmail.com>
2024-03-03 15:52:21 +0300

    created testlib reg test example

--- _apps/itcoty_web/testlib/client.py
+++ _apps/itcoty_web/testlib/client.py
@@ -1,63 +1,14 @@
 from dataclasses import dataclass
+from http import HTTPStatus
 
 import orjson
 import requests
-from pydantic import BaseModel, Extra
 
 
-class MyModel(BaseModel):
-    class Meta:
-        extra = Extra.forbid
-        json_dumps = orjson.dumps
-        json_loads = orjson.loads
-        orm_mode = True
-        validate_assignment = True
-
-
-class TokenPairResponse(MyModel):
-    access: str
-    refresh: str
-
-
-class VerifyTokenResponse(MyModel):
-    detail: str = "OK"
-
-
-class Authority(MyModel):
+@dataclass
+class RandomUser:
+    email: str
     password: str
-    username: str
-
-
-class RegisterResponse(MyModel):
-    message: str
-    user: Authority
-
-
-class ActivateResponse(MyModel):
-    message: str
-
-
-class InitiatePassowrdResetResponse(MyModel):
-    message: str
-    pass_secret_key: str
-    user: Authority
-
-
-class ConfirmPasswordResetResponse(MyModel):
-    message: str
-    pass_secret_key: str
-
-
-class ResetPasswordResponse(MyModel):
-    message: str
-
-
-class ResendCodeResponse(MyModel):
-    message: str
-
-
-class DeleteUserResponse(MyModel):
-    message: str
 
 
 @dataclass
@@ -65,75 +16,54 @@ class Client:
     host: str
     session: requests.Session
 
-    class ApiError(RuntimeError):
-        def __init__(
-            self,
-            *args: tuple,
-            message: str,
-            http_code: int,
-        ):
-            super().__init__(*args)
-            self.message = message
-            self.http_code = http_code
-
-        def __str__(self) -> str:
-            return f"message:{self.message}, http_code:{self.http_code}"
-
     def register(
         self,
         *,
         password: str,
-        username: str,
-    ) -> RegisterResponse:
+        email: str,
+    ) -> dict:
         response = self.session.post(
-            f"{self.host}/api/signup/",
+            f"{self.host}/api/v1/registration/",
             data=orjson.dumps(
                 {
-                    "password": password,
+                    "password1": password,
                     "password2": password,
-                    "username": username,
+                    "email": email,
                 }
             ),
         )
-        assert response.ok, response.text
-        payload = RegisterResponse.parse_raw(response.text)
-        return payload
+        assert response.status_code == HTTPStatus.CREATED, response.text
+        return response.json()
 
     def activate(
         self,
         *,
-        otp_code: str,
-        username: str,
-    ) -> ActivateResponse:
+        key: str,
+    ) -> dict:
         response = self.session.post(
-            f"{self.host}/api/signup_complete/",
+            f"{self.host}/api/v1/registration/verify-email/",
             data=orjson.dumps(
                 {
-                    "otp_code": otp_code,
-                    "username": username,
+                    "key": key,
                 }
             ),
         )
-        assert response.ok, response.text
-        payload = ActivateResponse.parse_raw(response.text)
-        return payload
+        assert response.status_code == HTTPStatus.OK, response.text
+        return response.json()
 
     def authenticate(
         self,
         *,
         password: str,
         username: str,
-    ) -> TokenPairResponse:
+    ) -> dict:
         response = self.session.post(
-            f"{self.host}/api/login/",
+            f"{self.host}/api/v1/login/",
             data={
                 "password": password,
                 "username": username,
             },
-            headers={
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
         )
         assert response.ok, response.text
-        payload = TokenPairResponse.parse_raw(response.text)
-        return payload
+
+        return response.json()
